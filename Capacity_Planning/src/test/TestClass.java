b425f1142156bade77371a3c07d5f60a4f8c06a0
Ramona Zauner <ramona.zauner@feld-m.de>
2017-11-04 10:31:16 +0100

    new test package to test several stuff

---
+++ Capacity_Planning/src/test/TestClass.java
@@ -0,0 +1,20 @@
+package test;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		double d = 1.9;
+		
+		int i = (int) d;
+		
+		System.out.println(i);
+		
+		
+		
+		
+		
+	}
+	
+	
+}
